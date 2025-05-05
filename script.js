282bdcfa885bbf556f69fecd00a74f88baed4f35
akshayasgo <124883220+akshayasgo@users.noreply.github.com>
2025-05-05 15:38:34 +0000

    adding data

--- script.js
+++ script.js
@@ -7,3 +7,43 @@
 //         });
 //     });
 // });
+
+const carouselDiv = document.querySelector('.jungle-carousel');
+
+fetch('carasoul_zone/index.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('card-carosol').innerHTML = html;
+  });
+
+const pricediv = document.querySelector('.price');
+
+fetch('price/index.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('price').innerHTML = html;
+  });
+
+const zonedetails = document.querySelector('.zone-details');
+
+fetch('zone_details/index.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('zone-details').innerHTML = html;
+  });
+
+const pannawelcome = document.querySelector('.panna-welcome');
+
+fetch('panna_welcome/index.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('panna-welcome').innerHTML = html;
+  });
+
+const connectivity = document.querySelector('.connectivity');
+
+fetch('connectivity/index.html')
+.then(response => response.text())
+.then(html => {
+  document.getElementById('connectivity').innerHTML = html;
+});
