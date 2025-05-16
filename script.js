34a92a7c398475e8102ff7b9e1b65101931c45e5
akshayasgo <124883220+akshayasgo@users.noreply.github.com>
2025-05-16 06:23:36 +0000

    addin code

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ if (safariDateInput) {
         });
     
         // Send data to  Apps Script or your server
-        fetch(mail_trigger1, {
+        fetch("https://script.google.com/macros/s/AKfycby4VYwZ7H6mlWEvBpvTUI_OB4vIMu7IcLxlrPXEETUchgsdu5FAyrw-GiHwYs5sut2u/exec", {
             method: "POST",
             mode: "no-cors",
             headers: {
